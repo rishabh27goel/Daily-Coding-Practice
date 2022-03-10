c25d30fb86d5488204842bcb08fdf28d910d99b5
Rishabh Goel <rishabhgoel2705@gmail.com>
2022-03-11 01:20:04 +0530

    11-mar-update

--- DSA_Sheet/Linked List/Clone_a_Linked_List_with_next_and_random_pointer.cpp
+++ DSA_Sheet/Linked List/Clone_a_Linked_List_with_next_and_random_pointer.cpp
@@ -37,8 +37,8 @@ Node* addNode(Node* head, Node* &tail, int data){
     return head;
 }
 
-// Method 1 : Using Map for storing old and new nodes
-// Time Complexity : O(n)  Space Complexity : O(n)
+// Method 1 : Checking for random node using additional loop
+// Time Complexity : O(n ^ 2)  Space Complexity : O(1)
 Node* createNode(Node* head, Node* &tail, int data){
 
     Node* newNode = new Node(data);
@@ -62,37 +62,150 @@ Node* cloneLLWithNextRandom(Node* head){
     if(head == NULL) return head;
 
     // First create copy for next pointer 
-    map<Node *, Node *> mapping;   // {mapping oldNode to newNode}
     Node* listHead = NULL;
     Node* listTail = NULL;
+    Node* curr1 = head;
 
-    Node* curr = head;
-    while(curr != NULL){
+    while(curr1 != NULL){
 
-        listHead = createNode(listHead, listTail, curr->data);
-        
-        mapping[curr] = listTail;
-        curr = curr->next;
+        listHead = createNode(listHead, listTail, curr1->data);
+        curr1 = curr1->next;
     }
 
-    // Handling random pointers
-    curr = head;
-    while(curr != NULL){
+    // Handling Random Pointer
+    Node* curr2 = listHead;
+    curr1 = head;
 
-        if(curr->random != NULL){
+    while(curr1 != NULL){
 
-            mapping[curr]->random = mapping[curr->random];
+        if(curr1->random != NULL){
+
+            // Reach the random node
+            Node* store = curr1->random;
+            Node* itr1 = head;
+            Node* itr2 = listHead;
+
+            while(itr1 != store){
+
+                itr2 = itr2->next;
+                itr1 = itr1->next;
+            }
+
+            curr2->random = itr2;
         }
         else{
 
-            mapping[curr]->random = NULL;
+            curr2->random = NULL;
         }
 
-        curr = curr->next;
+        curr1 = curr1->next;
+        curr2 = curr2->next;
     }
 
     return listHead;
 }
+
+// Method 2 : Using Map for storing old and new nodes
+// Time Complexity : O(n)  Space Complexity : O(n)
+// Node* cloneLLWithNextRandom(Node* head){
+
+//     if(head == NULL) return head;
+
+//     // First create copy for next pointer 
+//     map<Node *, Node *> mapping;   // {mapping oldNode to newNode}
+//     Node* listHead = NULL;
+//     Node* listTail = NULL;
+
+//     Node* curr = head;
+//     while(curr != NULL){
+
+//         listHead = createNode(listHead, listTail, curr->data);
+        
+//         mapping[curr] = listTail;
+//         curr = curr->next;
+//     }
+
+//     // Handling random pointers
+//     curr = head;
+//     while(curr != NULL){
+
+//         if(curr->random != NULL){
+
+//             mapping[curr]->random = mapping[curr->random];
+//         }
+//         else{
+
+//             mapping[curr]->random = NULL;
+//         }
+
+//         curr = curr->next;
+//     }
+
+//     return listHead;
+// }
+
+// Method 3 : Modifying original list without using extra space
+// Time Complexity : O(n)  Space Complexity : O(1)
+// Node* cloneLLWithNextRandom(Node* head){
+
+//     if(head == NULL) return head;
+
+//     // First create copy for next pointer and 
+//     // connect new list with original list
+//     Node* track = NULL;
+//     Node* curr = head;
+
+//     while(curr != NULL){
+
+//         track = curr->next;
+
+//         Node* newNode = new Node(curr->data);
+//         curr->next = newNode;
+//         newNode->next = track;
+
+//         curr = track;
+//     }
+
+//     // Handle random pointer
+//     curr = head;
+//     while(curr != NULL){
+
+//         if(curr->random != NULL){
+
+//             curr->next->random = curr->random->next;
+//         }
+//         else{
+
+//             curr->next->random = NULL;
+//         }
+
+//         curr = curr->next->next;
+//     }
+
+//     // Remove extra links 
+//     Node* newList = head->next;
+//     curr = head;
+//     track = NULL;
+
+//     while(curr != NULL){
+
+//         track = curr->next->next;
+
+//         if(track != NULL){
+
+//             curr->next->next = track->next;
+//         }
+//         else{
+
+//             curr->next->next = NULL;
+//         }
+
+//         curr->next = track;
+//         curr = track;
+//     }
+
+//     return newList;
+// }
 
 int main()
 {
@@ -157,7 +270,7 @@ int main()
     Node* newList = cloneLLWithNextRandom(listHead); 
 
     curr = newList;   
-    cout << "New List : \n";
+    cout << "\nNew List : \n";
     while(curr != NULL){
 
         cout << curr->data << " ";
