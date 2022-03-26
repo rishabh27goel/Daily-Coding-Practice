5f12cde37ce77264cbdde3163afdd2a4c8a6a7b2
Rishabh Goel <rishabhgoel2705@gmail.com>
2022-03-26 23:34:26 +0530

    26-mar-update

--- DSA_Sheet/Stack/Largest_rectangular_area_in_Histogram.cpp
+++ DSA_Sheet/Stack/Largest_rectangular_area_in_Histogram.cpp
@@ -1,47 +1,134 @@
 #include <iostream>
+#include <vector>
+#include <stack>
 using namespace std;
 
 // Method 1 : Simple Iteration to left and right (Brute Force)
 // Time Complexity : O(n^2)  Space Complexity : O(1)
+// int largestRectangularArea(int *arr, int n){
+
+//     if(n == 1) return arr[0];
+
+//     int maxArea = INT_MIN;
+//     int area;
+
+//     // Iterate Histogram
+//     for(int i=0; i<n; i++){
+
+//         int count = 1;
+
+//         // Left Check
+//         for(int j=i-1; j>=0; j--){
+
+//             if(arr[j] < arr[i]){
+
+//                 break;
+//             }
+//             else{
+
+//                 count++;
+//             }
+//         }
+
+//         // Right Check
+//         for(int j=i+1; j<n; j++){
+
+//             if(arr[j] < arr[i]){
+
+//                 break;
+//             }
+//             else{
+
+//                 count++;
+//             }
+//         }
+
+//         area = (arr[i] * count);
+
+//         if(area > maxArea){
+
+//             maxArea = area;
+//         }
+//     }
+
+//     return maxArea;
+// }
+
+// Method 2 : 
+// Time Complexity : O(n)  Space Complexity : O(n)
+void getNextSmaller(int *arr, int n, vector<int> &nextSmaller){
+
+    stack<int> st;
+
+    for(int i=n-1; i>=0; i--){
+
+        while(!st.empty() && arr[st.top()] >= arr[i]){
+
+            st.pop();
+        }
+
+        if(st.empty()){
+
+            st.push(i);
+        }
+        else{
+
+            nextSmaller[i] = st.top();
+            st.push(i);
+        }
+    }
+}
+
+void getPrevSmaller(int *arr, int n, vector<int> &prevSmaller){
+
+    stack<int> st;
+
+    for(int i=0; i<n; i++){
+
+        while(!st.empty() && arr[st.top()] >= arr[i]){
+
+            st.pop();
+        }
+
+        if(st.empty()){
+
+            st.push(i);
+        }
+        else{
+
+            prevSmaller[i] = st.top();
+            st.push(i);
+        }
+    }
+}
+
 int largestRectangularArea(int *arr, int n){
 
     if(n == 1) return arr[0];
 
     int maxArea = INT_MIN;
+    vector<int> nextSmaller(n, n);
+    vector<int> prevSmaller(n, -1);
+
+    // Next Smaller Element
+    getNextSmaller(arr, n, nextSmaller);
+
+    // Prev Smaller Element
+    getPrevSmaller(arr, n, prevSmaller);
+
+    // Finding Largest Area
     int area;
 
-    // Iterate Histogram
     for(int i=0; i<n; i++){
 
-        int count = 1;
+        if(prevSmaller[i] != -1){
 
-        // Left Check
-        for(int j=i-1; j>=0; j--){
-
-            if(arr[j] < arr[i]){
-
-                break;
-            }
-            else{
-
-                count++;
-            }
+            area = (arr[i] * (nextSmaller[i] - prevSmaller[i] - 1));
         }
+        else{
 
-        // Right Check
-        for(int j=i+1; j<n; j++){
-
-            if(arr[j] < arr[i]){
-
-                break;
-            }
-            else{
-
-                count++;
-            }
+            area = (arr[i] * (nextSmaller[i] + prevSmaller[i] + 1));
         }
-
-        area = (arr[i] * count);
 
         if(area > maxArea){
 
@@ -50,12 +137,6 @@ int largestRectangularArea(int *arr, int n){
     }
 
     return maxArea;
-}
-
-// Method 2 : 
-// Time Complexity : O(n)  Space Complexity : O(n)
-int largestRectangularArea(int *arr, int n){
-
 }
 
 int main()
