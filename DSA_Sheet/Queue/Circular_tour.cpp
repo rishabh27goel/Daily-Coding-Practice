17c2d494ec9f9c9f5b00ccc9562d835f73a3441e
Rishabh Goel <rishabhgoel2705@gmail.com>
2022-04-11 22:20:06 +0530

    11-apr-update

--- DSA_Sheet/Queue/Circular_tour.cpp
+++ DSA_Sheet/Queue/Circular_tour.cpp
@@ -36,45 +36,79 @@ using namespace std;
 
 // Method 2 : Optimised from Above [Circular Queue Based]
 // Time Complexity : O(n ^ 2)   Space Complexity : O(1)
+// int circularTour(vector<int> petrol, vector<int> distance){
+
+//     int tank = 0;
+//     int start = 0;
+//     int travel = 0;
+
+//     while(start < petrol.size()){
+
+//         tank = tank + petrol[travel] - distance[travel];
+
+//         if(tank < 0){
+
+//             tank = 0;
+
+//             // If start to travel not possible then there is no
+//             // point of traversing till travel we will start from travel + 1
+
+//             // Reason : If tank + petrol[travel] - distance[travel] is negative then
+//             // petrol[travel] - distance[travel] will always be negative [And can not happen]
+
+//             if(travel < start){
+
+//                 start = start + 1;
+//             }
+//             else{
+
+//                 start = travel + 1;
+//             }
+
+//             travel = start;
+
+//             continue;
+//         }
+//         else{
+
+//             travel = (travel + 1) % petrol.size();
+//         }
+
+//         if(start == travel) return start;
+//     }
+
+//     return -1;
+// }
+
+// Method 3 : Single Tour [Keep track of Deficit]
+// Time Complexity : O(n)   Space Complexity : O(1)
 int circularTour(vector<int> petrol, vector<int> distance){
 
     int tank = 0;
+    int deficit = 0;
+
     int start = 0;
     int travel = 0;
-
-    while(start < petrol.size()){
+    
+    while(travel < petrol.size()){
 
         tank = tank + petrol[travel] - distance[travel];
 
         if(tank < 0){
 
+            // Deficit is keeping track of all petrol needed till travel index 
+            // [Can be individual or combination of some pumps]
+            deficit += tank;
             tank = 0;
-
-            // If start to travel not possible then there is no
-            // point of traversing till travel we will start from travel + 1
-
-            // Reason : If tank + petrol[travel] - distance[travel] is negative then
-            // petrol[travel] - distance[travel] will always be negative [And can not happen]
-
-            if(travel < start){
-
-                start = start + 1;
-            }
-            else{
-
-                start = travel + 1;
-            }
-
-            travel = start;
-
-            continue;
-        }
-        else{
-
-            travel = (travel + 1) % petrol.size();
+            start = travel + 1;
         }
 
-        if(start == travel) return start;
+        travel = travel + 1;
+    }
+
+    if(tank + deficit >= 0){
+
+        return start;
     }
 
     return -1;
