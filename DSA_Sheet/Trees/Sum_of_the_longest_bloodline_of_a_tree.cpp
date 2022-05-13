9ccf287f1209cd4a453e8dfb730a72402a48f993
Rishabh Goel <rishabhgoel2705@gmail.com>
2022-05-13 23:35:42 +0530

    13-may-update-2

--- DSA_Sheet/Trees/Sum_of_the_longest_bloodline_of_a_tree.cpp
+++ DSA_Sheet/Trees/Sum_of_the_longest_bloodline_of_a_tree.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <utility>
+#include <queue>
 using namespace std;
 
 class Node {
@@ -32,37 +33,107 @@ Node* createTree(Node* root){
     return root;
 }
 
-// Method : Recursive Method 
+// Method 1: Recursive Method 
 // Time Complexity : O(n)  Space Complexity : O(n)
-void helperBloodline(Node* root, int length, int pathSum, pair<int, int> &pr){
+// void helperBloodline(Node* root, int length, int pathSum, pair<int, int> &pr){
 
-    // Empty Node
-    if(root == NULL) return;
+//     // Empty Node
+//     if(root == NULL) return;
 
-    // Leaf Node
-    if(root->left == NULL && root->right == NULL){
+//     // Leaf Node
+//     if(root->left == NULL && root->right == NULL){
 
-        // Check when length is higher
-        if(length >= pr.first){
+//         // Check when length is higher
+//         if(length > pr.first){
 
-            pr.first = length;
-            pr.second = max(pathSum + root->data, pr.second);
-        }
+//             pr.first = length;
+//             pr.second = pathSum + root->data;
+//         }
+//         else if(length == pr.first){
 
-        return;
+//             pr.second = max(pr.second, pathSum + root->data);
+//         }
+
+//         return;
+//     }
+
+//     helperBloodline(root->left, length + 1, pathSum + root->data, pr);
+//     helperBloodline(root->right, length + 1, pathSum + root->data, pr);    
+// }
+
+// int sumLongestBloodline(Node* root){
+
+//     if(root == NULL) return 0;
+
+//     pair<int, int> pr = make_pair(0, 0);
+
+//     helperBloodline(root, 1, 0, pr);
+
+//     return pr.second;
+// }
+
+// Method 2: Iterative Method 
+// Time Complexity : O(n)  Space Complexity : O(n)
+class Triplet {
+
+    public:
+        Node* node;
+        int length;
+        int sum;
+
+    Triplet(Node* node, int length, int sum){
+
+        this->node = node;
+        this->length = length;
+        this->sum = sum;
     }
-
-    helperBloodline(root->left, length + 1, pathSum + root->data, pr);
-    helperBloodline(root->right, length + 1, pathSum + root->data, pr);    
-}
+};
 
 int sumLongestBloodline(Node* root){
 
     if(root == NULL) return 0;
 
-    pair<int, int> pr = make_pair(0, 0);
+    // Store tree nodes
+    pair<int, int> pr = make_pair(1, 0);
 
-    helperBloodline(root, 1, 0, pr);
+    queue<Triplet*> q;
+    q.push(new Triplet(root, 1, 0));
+
+    while(!q.empty()){
+
+        Triplet* trp = q.front();
+        q.pop();
+
+        Node* front = trp->node;
+        int length = trp->length;
+        int sum = trp->sum;
+
+        // Update Sum of Leaf Node
+        if(front->left == NULL && front->right == NULL){
+
+            if(length > pr.first){
+
+                pr.first = length;
+                pr.second = max(sum + front->data, pr.second);
+            }
+            else if(length == pr.first){
+                
+                pr.second = max(sum + root->data, pr.second);
+            }
+        }
+
+        // Left Child
+        if(front->left != NULL){
+
+            q.push(new Triplet(front->left, length + 1, sum + front->data));
+        }
+
+        // Right Child
+        if(front->right != NULL){
+
+            q.push(new Triplet(front->right, length + 1, sum + front->data));
+        }
+    }
 
     return pr.second;
 }
