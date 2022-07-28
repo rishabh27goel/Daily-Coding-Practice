d027ac47361c960a99778d331e50b1a7f7c8448b
Rishabh Goel <rishabhgoel2705@gmail.com>
2022-07-28 12:12:48 +0530

    28-jul-update

--- DSA_Sheet/Graph/Detect_Cycle_in_Directed_Graph.cpp
+++ DSA_Sheet/Graph/Detect_Cycle_in_Directed_Graph.cpp
@@ -1,72 +1,121 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <queue>
 using namespace std;
 
 // Method 1 : Depth First Search
 // Time Complexity : O(V + E)  Space Complexity : O(V + E)
-bool cycleHelper(unordered_map <int, vector<int> > &adjList, unordered_map <int, int> &visited, unordered_map <int, int> &dfsExits, int start){
+// bool cycleHelper(unordered_map <int, vector<int> > &adjList, unordered_map <int, int> &visited, unordered_map <int, int> &dfsExits, int start){
 
-    visited[start] = true;
-    dfsExits[start] = true;
+//     visited[start] = true;
+//     dfsExits[start] = true;
 
-    for(int i=0; i<adjList[start].size(); i++){
+//     for(int i=0; i<adjList[start].size(); i++){
 
-        if(visited[adjList[start][i]] == false){
+//         if(visited[adjList[start][i]] == false){
 
-            // If we have cycle on this path
-            if(cycleHelper(adjList, visited, dfsExits, adjList[start][i])){
+//             // If we have cycle on this path
+//             if(cycleHelper(adjList, visited, dfsExits, adjList[start][i])){
 
-                return true;
-            }
-        }
-        else{
+//                 return true;
+//             }
+//         }
+//         else{
 
-            // If node is in the visited map
-            // And it is in the recursion stack
-            if(dfsExits[adjList[start][i]] == true){
+//             // If node is in the visited map
+//             // And it is in the recursion stack
+//             if(dfsExits[adjList[start][i]] == true){
 
-                // Loops Exits
-                return true;
-            }
-        }
-    }
+//                 // Loops Exits
+//                 return true;
+//             }
+//         }
+//     }
 
-    // Call remove from recursion stack
-    dfsExits[start] = false;
+//     // Call remove from recursion stack
+//     dfsExits[start] = false;
 
-    return false;
-}
+//     return false;
+// }
 
+// bool detectCycle(int n, vector< pair<int, int> > &edges){
+
+//     // Create Adjacency List
+//     unordered_map <int, vector<int> > adjList;
+
+//     for(int i=0; i<edges.size(); i++){
+
+//         adjList[edges[i].first].push_back(edges[i].second);
+//     }
+
+//     // Visited and Recursion Track of Start
+//     unordered_map <int, int> visited;
+//     unordered_map <int, int> dfsExits;
+
+//     for(int i=1; i<=n; i++){
+
+//         if(visited[i] == false){
+
+//             // If cycle exits with ith node
+//             if(cycleHelper(adjList, visited, dfsExits, i)){
+                
+//                 return true;
+//             }
+//         }
+//     }
+
+//     return false;
+// }
+
+// Method 2 : Breadth First Search
+// Time Complexity : O(V + E)  Space Complexity : O(V + E)
 bool detectCycle(int n, vector< pair<int, int> > &edges){
 
-    // Create Adjacency List
+    // Create Adjacency List & Use Kahn's Algorithm
     unordered_map <int, vector<int> > adjList;
-
+    vector<int> indegree(n, 0);
+    
     for(int i=0; i<edges.size(); i++){
 
         adjList[edges[i].first].push_back(edges[i].second);
+        indegree[edges[i].second]++;
     }
 
-    // Visited and Recursion Track of Start
-    unordered_map <int, int> visited;
-    unordered_map <int, int> dfsExits;
+    int total = 0;
+    queue <int> q;
 
-    for(int i=1; i<=n; i++){
+    // Push node with in-degree 0
+    for(int i=0; i<n; i++){
 
-        if(visited[i] == false){
+        if(indegree[i] == 0)
+            q.push(i);
+    }
 
-            // If cycle exits with ith node
-            if(cycleHelper(adjList, visited, dfsExits, i)){
-                
-                return true;
-            }
+    while(!q.empty()){
+
+        int front = q.front();
+        q.pop();
+
+        total++;
+
+        for(int i=0; i<adjList[front].size(); i++){
+
+            indegree[adjList[front][i]]--;
+
+            if(indegree[adjList[front][i]] == 0)
+                q.push(adjList[front][i]);
         }
+
     }
 
-    return false;
-}
+    // No Loop Exits
+    if(total == n)
+        return false;
 
+
+    return true;
+}
 
 int main()
 {
