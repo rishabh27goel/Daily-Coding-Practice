400a74f49a078b62ed24c27826f482b263942eff
Rishabh Goel <rishabhgoel2705@gmail.com>
2022-09-28 19:04:55 +0530

    28-sep-update

---
+++ DSA_Sheet/Dynamic Programming/1D DP/Nth_catalan_number.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Method 1 : 
+// Time Complexity : O()  Space Complexity : O()
+int nthCatalanNumber(int n){
+
+    
+}
+
+int main()
+{
+    cout << "Enter n : ";
+    int n;
+    cin >> n;
+
+    // Nth Catalan Number
+    int value = nthCatalanNumber(n);
+
+    cout << "Nth Catalan Numbecr : " << value;
+
+
+    cout << endl;
+    return 0;
+}
